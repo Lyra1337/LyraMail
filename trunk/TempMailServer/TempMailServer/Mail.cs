81928e54fcaf5a748d954c9b052c75743c0a6b56
Lyra <Lyra@c9d61f84-e345-2c41-8df5-9abaacd4a82c>
2011-12-12 13:46:28 +0000

    MailBody Parser should be done

--- trunk/TempMailServer/TempMailServer/Mail.cs
+++ trunk/TempMailServer/TempMailServer/Mail.cs
@@ -14,9 +14,15 @@ namespace Lyralabs.Net.TempMailServer
     private StringBuilder body = null;
     private Dictionary<string, List<string>> headers = null;
 
+    public List<MailBodyPart> BodyParts
+    {
+      get;
+      set;
+    }
+
     public Mail(string _rawContent)
     {
-      if (String.IsNullOrEmpty(_rawContent))
+      if(String.IsNullOrEmpty(_rawContent))
         throw new ArgumentNullException("rawContent is null!");
 
       this.rawContent = _rawContent;
@@ -27,17 +33,107 @@ namespace Lyralabs.Net.TempMailServer
     private void ParseBody()
     {
       string contentType = null;
-      if (this.headers.ContainsKey("Content-Type"))
+      string boundary = null;
+      bool multipart = false;
+
+      if(this.headers.ContainsKey("Content-Type"))
       {
         List<string> c = this.headers["Content-Type"];
-        if (c != null && c.Count > 0)
+        if(c != null && c.Count > 0)
         {
           Match m = Mail.contentTypeParser.Match(c[0]);
-          if (m.Success)
+          if(m.Success)
           {
-            string boundary = m.Groups["boundary"].Value;
+            boundary = m.Groups["boundary"].Value;
             contentType = m.Groups["type"].Value;
+            multipart = true;
           }
+        }
+      }
+
+      if(multipart)
+      {
+        #region comments
+        /*      Dictionary<string, List<string>> lines = new Dictionary<string, List<string>>();
+        string currentContentType = null;
+        bool isHeader = false;
+        int partCounter = 0;
+        foreach(string line in this.body.ToString().Replace("\r", "").Split('\n'))
+        {
+          if(String.IsNullOrEmpty(line))
+          {
+            isHeader = false;
+            if(currentContentType == null)
+            {
+              continue;
+            }
+            else
+            {
+              if(lines.ContainsKey(currentContentType) == false)
+                lines.Add(currentContentType, new List<string>());
+
+              lines[currentContentType].Add("");
+            }
+          }
+          else
+          {
+            if(isHeader)
+            {
+              if(line.StartsWith("Content-Type:"))
+              {
+                string[] cType = line.Split(' ');
+                if(cType.Length > 1)
+                {
+                  currentContentType = cType[1];
+                }
+              }
+            }
+            if(line.Trim() == String.Concat("--", boundary))
+            {
+              isHeader = true;
+
+              if(lines.ContainsKey(currentContentType) == false)
+                lines.Add(currentContentType, new List<string>());
+
+              lines[currentContentType].Add("");
+            }
+            else if(line.Trim() == String.Concat("--", boundary, "--"))
+            {
+              break;
+            }
+          }
+        }*/
+        #endregion
+
+        List<List<string>> parts = null;
+        List<MailBodyPart> mailBodyParts = new List<MailBodyPart>();
+
+        foreach(string line in this.body.ToString().Split('\n'))
+        {
+          if(line.StartsWith(String.Concat("--", boundary)))
+          {
+            if(line.EndsWith(String.Concat(boundary, "--")))
+              break;
+
+            if(parts == null)
+              parts = new List<List<string>>();
+
+            parts.Add(new List<string>());
+          }
+          else
+          {
+            if(parts == null)
+              continue;
+            parts[parts.Count - 1].Add(line);
+          }
+        }
+
+        foreach(List<string> part in parts)
+        {
+          if(this.BodyParts == null)
+            this.BodyParts = new List<MailBodyPart>();
+
+          this.BodyParts.Add(new MailBodyPart(part));
         }
       }
     }
@@ -48,15 +144,15 @@ namespace Lyralabs.Net.TempMailServer
       bool header = true;
       this.headers = new Dictionary<string, List<string>>();
 
-      foreach (string line in this.rawContent.Replace("\r", "").Split('\n'))
+      foreach(string line in this.rawContent.Replace("\r", "").Split('\n'))
       {
-        if (header)
+        if(header)
         {
-          if (String.IsNullOrEmpty(line))
+          if(String.IsNullOrEmpty(line))
           {
             header = false;
           }
-          else if (line.StartsWith(" ") && this.headers.Count > 0)
+          else if(line.StartsWith(" ") && this.headers.Count > 0)
           {
             this.headers[this.headers.Keys.Last()][this.headers[this.headers.Keys.Last()].Count - 1] += line.Substring(1, line.Length - 1).Trim();
           }
@@ -66,7 +162,7 @@ namespace Lyralabs.Net.TempMailServer
             int separator = line.IndexOf(':');
             key = line.Substring(0, separator);
             value = line.Substring(separator + 1, line.Length - (separator + 1)).TrimStart();
-            if (this.headers.ContainsKey(key) == false)
+            if(this.headers.ContainsKey(key) == false)
             {
               this.headers.Add(key, new List<string>());
             }
---
+++ trunk/TempMailServer/TempMailServer/MailBodyPart.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Lyralabs.Net.TempMailServer
+{
+  class MailBodyPart
+  {
+    public MailBodyPart(List<string> bodyLines)
+    {
+      this.ParseBody(bodyLines);
+    }
+
+    private void ParseBody(List<string> bodyLines)
+    {
+      bool isHeader = true;
+      StringBuilder body = new StringBuilder();
+      foreach(string line in bodyLines)
+      {
+        if(isHeader)
+        {
+          if(String.IsNullOrWhiteSpace(line))
+          {
+            isHeader = false;
+          }
+          else
+          {
+            if(line.StartsWith("Content-Type:"))
+            {
+              string[] parts = line.Split(' ');
+              switch(parts.Length)
+              {
+                case 2:
+                  this.MimeType = parts[1];
+                  this.Encoding = null;
+                  break;
+                case 3:
+                  this.MimeType = parts[1];
+                  this.Encoding = parts[2];
+                  break;
+                default:
+                  break;
+              }
+            }
+          }
+        }
+        else
+        {
+          body.AppendLine(line);
+        }
+      }
+      this.BodyText = body.ToString();
+    }
+
+    public string MimeType
+    {
+      get;
+      set;
+    }
+    public string Encoding
+    {
+      get;
+      set;
+    }
+    public string BodyText
+    {
+      get;
+      set;
+    }
+  }
+}
