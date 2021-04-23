02ae932de148747a764a08ac93ee6d7aa17d9538
Lyra <github@lyra.bz>
2021-04-23 14:25:38 +0200

    * removed windows specific event logging

--- TempMailServer/TempMailServer/MailServer.cs
+++ TempMailServer/TempMailServer/MailServer.cs
@@ -15,11 +15,8 @@ namespace Lyralabs.Net.TempMailServer
     public class MailServer
     {
         private static readonly int SERVER_PORT = 25;
-        private static readonly string LOG_SOURCE_NAME = "GWS MailReceiver";
-        private static readonly string LOG_NAME = "GWS MailReceiver";
 
         protected TcpListener ServerSocket = null;
-        protected EventLog Log = null;
 
         public delegate void ChangedEventHandler(object sender, MailReceivedEventArgs e);
         public event ChangedEventHandler MailReceivedEvent = null;
@@ -43,13 +40,6 @@ namespace Lyralabs.Net.TempMailServer
 
         private void Prepare()
         {
-            if (EventLog.SourceExists(MailServer.LOG_SOURCE_NAME) == false)
-            {
-                EventLog.CreateEventSource(MailServer.LOG_SOURCE_NAME, MailServer.LOG_NAME);
-            }
-
-            this.Log = new EventLog(MailServer.LOG_NAME, ".", MailServer.LOG_SOURCE_NAME);
-
             this.Mails = new List<Mail>();
         }
 
@@ -59,16 +49,21 @@ namespace Lyralabs.Net.TempMailServer
             this.ServerSocket = new TcpListener(IPAddress.Any, MailServer.SERVER_PORT);
             this.ServerSocket.Start();
 
-            this.Log.WriteEntry(String.Format("Mailserver started at port {0}", MailServer.SERVER_PORT), EventLogEntryType.Information);
+            this.LogInformation(String.Format("Mailserver started at port {0}", MailServer.SERVER_PORT));
 
             while (true)
             {
                 TcpClient clientSocket = this.ServerSocket.AcceptTcpClient();
-                this.Log.WriteEntry(String.Format("Connected! [IP: {0}]", clientSocket.Client.RemoteEndPoint), EventLogEntryType.Information);
+                this.LogInformation(String.Format("Connected! [IP: {0}]", clientSocket.Client.RemoteEndPoint));
 
                 Thread thread = new Thread(new ParameterizedThreadStart(this.ProcessConnection));
                 thread.Start(clientSocket);
             }
+        }
+
+        private void LogInformation(string text)
+        {
+            // TODO: Add logging
         }
 
         private void ProcessConnection(object objTcpClient)
