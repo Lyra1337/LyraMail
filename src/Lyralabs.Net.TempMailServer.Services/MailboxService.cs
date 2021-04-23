7234d7486e09e50203e3e8635738f5b9027a5ad1
Lyra <github@lyra.bz>
2021-04-23 17:06:51 +0200

    * added services project

---
+++ src/Lyralabs.Net.TempMailServer.Services/MailboxService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lyralabs.Net.TempMailServer.Services
+{
+    public class Email { }
+
+    public class MailboxService
+    {
+        public List<Email> GetMails(string account)
+        {
+            return null;
+        }
+    }
+}
