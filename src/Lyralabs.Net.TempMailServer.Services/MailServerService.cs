c2bd82d927e7df82e173564d72df6e1df015ba9b
Lyra <github@lyra.bz>
2021-04-24 03:41:17 +0200

    * added debug-message store and mail server service

---
+++ src/Lyralabs.Net.TempMailServer.Services/MailServerService.cs
@@ -0,0 +1,25 @@
+﻿using SmtpServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyralabs.Net.TempMailServer.Services
+{
+    class MailServerService
+    {
+        public MailServerService()
+        {
+            var options = new SmtpServerOptionsBuilder()
+                .ServerName("localhost")
+                .Port(25, 587)
+                .Build();
+        }
+
+        public void Start()
+        {
+
+        }
+    }
+}
---
+++ src/Lyralabs.Net.TempMailServer.Services/TempMessageStore.cs
@@ -0,0 +1,39 @@
+﻿using SmtpServer;
+using SmtpServer.Protocol;
+using SmtpServer.Storage;
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lyralabs.Net.TempMailServer.Services
+{
+    class TempMessageStore : MessageStore
+    {
+        public override async Task<SmtpResponse> SaveAsync(
+            ISessionContext context,
+            IMessageTransaction transaction,
+            ReadOnlySequence<byte> buffer,
+            CancellationToken cancellationToken)
+        {
+            await using var stream = new MemoryStream();
+
+            var position = buffer.GetPosition(0);
+            while (buffer.TryGet(ref position, out var memory))
+            {
+                await stream.WriteAsync(memory, cancellationToken);
+            }
+
+            stream.Position = 0;
+
+            var message = await MimeKit.MimeMessage.LoadAsync(stream, cancellationToken);
+            Console.WriteLine(message.TextBody);
+
+            return SmtpResponse.Ok;
+        }
+    }
+}
