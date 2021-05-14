fe1fb57c387fd31f97bd3e85caa39e691e437248
Lyra <github@lyra.bz>
2021-05-15 00:20:03 +0200

    added migration for fixed secret column in mails table

--- src/Lyralabs.TempMailServer.Data.Context/Migrations/20210514221446_FixedSecretColumnInMails.Designer.cs
+++ src/Lyralabs.TempMailServer.Data.Context/Migrations/20210514221446_FixedSecretColumnInMails.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Lyralabs.TempMailServer.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Lyralabs.TempMailServer.Data.Context.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210514221446_FixedSecretColumnInMails")]
+    partial class FixedSecretColumnInMails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Lyralabs.TempMailServer.Data.Context/Migrations/20210514221446_FixedSecretColumnInMails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lyralabs.TempMailServer.Data.Context.Migrations
+{
+    public partial class FixedSecretColumnInMails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Secret",
+                table: "Mails",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: Guid.Parse("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Secret",
+                table: "Mails");
+        }
+    }
+}
