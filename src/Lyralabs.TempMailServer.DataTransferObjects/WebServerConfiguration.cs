8452a2b148f44aca605ab22d94eb56ce43600cfa
Lyra1337 <git@lyra.bz>
2021-09-29 02:33:37 +0200

    added webserver configuration

---
+++ src/Lyralabs.TempMailServer.DataTransferObjects/WebServerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyralabs.TempMailServer
+{
+    public class WebServerConfiguration
+    {
+        public string MailContentBaseUrl { get; set; }
+    }
+}
