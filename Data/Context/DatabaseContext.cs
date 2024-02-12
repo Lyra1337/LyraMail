6f52341e5ba68be65a3b9b6fd226a0c6693add87
Lyra1337 <git@lyra.bz>
2024-02-12 15:04:39 +0100

    fixed Identity DbContext config

--- Data/Context/DatabaseContext.cs
+++ Data/Context/DatabaseContext.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lyralabs.TempMailServer.Data.Context
 {
-    public class DatabaseContext : IdentityDbContext<UserModel, DatabaseContext>
+    public class DatabaseContext : IdentityDbContext<UserModel, IdentityRole<int>, int>
     {
         public DbSet<MailboxModel> Mailboxes { get; set; }
 
         public DbSet<MailModel> Mails { get; set; }
 
-        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        public DatabaseContext(DbContextOptions options)
             : base(options)
         { }
     }
